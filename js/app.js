569d52d63bc32215b2be066d48bbc8c6bba2ea68
freshmeatba11 <cmjj8898212032@gmail.com>
2021-06-30 02:30:20 +0800

    minify images file size, replace jquery(fadeOut) with JS+CSS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+//跳轉淡出動畫
+let body = document.querySelector("body");
+let links = document.querySelectorAll("a.link");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", (e) => {
+    //取消事件預設動作
+    e.preventDefault();
+    let link = e.target;
+    let newLocation = link.href;
+    //設定動畫效果
+    body.style.animation = "fadeOut 1s ease-out forwards";
+    //動畫結束執行跳轉
+    body.addEventListener("animationend", newPage);
+    //跳轉頁面
+    function newPage() {
+      window.location = newLocation;
+    }
+  });
+}
